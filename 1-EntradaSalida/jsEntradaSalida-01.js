0434c7d490eec4d90197e9e6caa2203e58b8b5ef
MarianoGuevara <marianoguevara2005@gmail.com>
2023-01-30 16:04:30 -0300

    ejercicios 1y2 de entrada y salida js con comentarios

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,7 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Mundo");
+	alert("Esto funciona de maravilla"); /*alert () crea una ventana emergente que muestra un valor que es el que
+											se encuentra dentro de los paréntesis, entre comillas */
 }
 
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,25 @@
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+
+
 function mostrar()
 {
-	alert("ok");
+	let variable1	//	la palabra let indica variable, y el "variable1" es el nombre de la variable en si
+	variable1 = "Valor asignado";	//	a la variable de nombre "variable1" le asignas el valor "valor asignado" con el=
+
+	alert (variable1);	//	con el alert mostras la variable1
+}
+
+//LA UNICA FUNCIÓN QUE TENDRÁ VALIDEZ EN EL CÓDIGO SERÁ LA QUE SEA ESCRITA ÚLTIMA, YA QUE EL PROGRAMA SE EJECUTA EN 
+//ORDEN Y LA ULTIMA FUNCION ES LA DEFINITIVA
+
+function mostrar ()
+{
+	let nombre = prompt("Escribe tu nombre");	//	prompt es una ventana emergente que pide datos al usuario
+	alert (nombre);
+
+	//aca estoy diciendo que el valor de mi variable nombre es prompt (lo que ingrese el usuario) 
+	//y con el alert, ejecutas la variable nombre, que ahora equivale al dato ingresado por el usuario gracias a prompt
 }
 
