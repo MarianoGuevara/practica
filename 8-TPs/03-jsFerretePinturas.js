d665293c44c5ee880a1ff81fcab8148995bf6cce
MarianoGuevara <marianoguevara2005@gmail.com>
2023-02-02 16:15:36 -0300

    ejs 1 y 3 de apartado tp, entrada y salida

--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -2,12 +2,29 @@
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
+
 function FahrenheitCentigrados () 
 {
-	
+	let temperaturaFahrenheit;
+    let temperaturaCentígrados;
+
+    temperaturaFahrenheit = document.getElementById("txtIdTemperatura").value;
+    temperaturaFahrenheit = parseInt(temperaturaFahrenheit);
+
+    temperaturaCentígrados = (temperaturaFahrenheit - 32) / 1.8;    //cuenta de conversion sacada de google
+   
+    alert(temperaturaFahrenheit + " grados fahrenheit son " + temperaturaCentígrados + " grados centígrados");
 }
 
 function CentigradosFahrenheit () 
 {
-	
+    let temperaturaCentígrados;
+    let temperaturaFahrenheit;
+   
+    temperaturaCentígrados = document.getElementById("txtIdTemperatura").value;
+    temperaturaCentígrados = parseInt(temperaturaCentígrados);
+
+    temperaturaFahrenheit = (temperaturaCentígrados * 9/5) + 32;    //cuenta de conversion sacada de google
+   
+    alert(temperaturaCentígrados + " grados centígrados son " + temperaturaFahrenheit + " grados fahrenheit");
 }
